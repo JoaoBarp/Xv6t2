3563b2daef2e84461290004b5da0dd70a10d01c1
JoaoBarp <joaorbarp@hotmail.com>
2016-11-25 09:12:15 -0200

    Arquivo teste-trabalho anterior

---
+++ teste.c
@@ -0,0 +1,30 @@
+#include "types.h"
+#include "user.h"
+#include "syscall.h" 
+ 
+ 
+void f1(void);
+
+int main (int argh,char *argv[]){
+	int p[argh];
+	int j;
+	for(j=0;j<argh;j++){      //Cria os filhos com a quantidade de argumeentos de "argh"
+	  p[j]=atoi(argv[j]);    //Converte char pra int
+	  if(fork(p[j])==0){     //Cria filhos
+	      f1();              //comendo tempo
+	      exit(); 
+     }
+    }
+
+	for(j=0;j<10000;j++) {
+		wait();             //esperando os filhos
+	}
+	exit();                 
+}
+
+void f1(void){
+	int i=0;	
+	while(i<5000000000){
+		i++;
+	}
+}
